46958dd877c756da0247726d1fa4fed114a25143
surely07 <sweetpt88@gmail.com>
2023-11-01 16:52:56 +0900

    과제완성

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const initialState = ["banana", "banana", "cherry", "date", "elderberry"];
+  const initialState = ["apple", "banana", "cherry", "date", "elderberry"];
   const [array, setArray] = useState(initialState);
   const [result, setResult] = useState(""); // result는 텍스트일 뿐
   const [query, setQuery] = useState(""); // input을 위한 value
-
-  // 너무 길어지는 것을 막기 위해 함수 부분을 위로 빼서 관리
 
   const handleForEach = function () {
     let temp = "";
@@ -19,12 +17,12 @@ function App() {
   };
 
   const handleFilter = function () {
-    const filtered = array.filter((item, index) => item.includes(query));
+    const filtered = array.filter((item) => item.includes(query));
     setResult(filtered.join(", "));
   };
 
   const handleMap = function () {
-    const mapped = array.map(function (item, index) {
+    const mapped = array.map(function (item) {
       return item.toUpperCase();
     });
     setResult(mapped.join(", "));
@@ -54,8 +52,54 @@ function App() {
     setResult(newArr.join(", "));
   };
 
+  const handleSlice = function () {
+    const sliced = array.slice(1, 4);
+    setResult(sliced.join(", "));
+  };
+
+  const handleSplice = function () {
+    const newArr = [...array];
+    newArr.splice(2, 2, "kiwi", "lime");
+    setArray(newArr);
+    setResult(newArr.join(", "));
+  };
+
+  const handleIndexOf = function () {
+    const indexed = array.indexOf(query);
+    setResult(indexed);
+  };
+
+  const handleIncludes = function () {
+    const included = array.includes(query);
+    setResult(included.toString());
+  };
+
+  const handleFind = function () {
+    const found = array.find((item) => item.includes(query));
+    setResult(found);
+  };
+
+  const handleSome = function () {
+    const some = array.some((item) => item.includes(query));
+    setResult(some.toString());
+  };
+
+  const handleEvery = function () {
+    const every = array.every((item) => item.includes(query));
+    setResult(every.toString());
+  };
+
+  const handleSort = function () {
+    const sorted = array.sort();
+    setResult(sorted.join(", "));
+  };
+
+  const handleJoin = function () {
+    setResult(array.join(", "));
+  };
+
   return (
-    <div>
+    <div class="App">
       <h1>Standard반 배열 API 테스트</h1>
       <input
         value={query}
@@ -67,16 +111,35 @@ function App() {
         <button onClick={handleForEach}>forEach</button>
         <button onClick={handleFilter}>filter</button>
         <button onClick={handleMap}>map</button>
-        <button onClick={handleReduce}>Reduce</button>
-        <button onClick={handlePush}>Push</button>
-        <button onClick={handlePop}>Pop</button>
+        <button onClick={handleReduce}>reduce</button>
+        <button onClick={handlePush}>push</button>
+        <button onClick={handlePop}>pop</button>
+        <button onClick={handleSlice}>slice</button>
+        <button onClick={handleSplice}>splice</button>
+        <button onClick={handleIndexOf}>indexOf</button>
+        <button onClick={handleIncludes}>includes</button>
+        <button onClick={handleFind}>find</button>
+        <button onClick={handleSome}>some</button>
+        <button onClick={handleEvery}>every</button>
+        <button onClick={handleSort}>sort</button>
+        <button onClick={handleJoin}>join</button>
       </div>
-      <div>
-        <h3>원본배열</h3>
+      <div
+        style={{
+          border: "1px solid black",
+          margin: "10px",
+        }}
+      >
+        <h3>원본배열 : </h3>
         <p>{array.join(", ")}</p>
       </div>
-      <div>
-        <h3>결과물</h3>
+      <div
+        style={{
+          border: "1px solid black",
+          margin: "10px",
+        }}
+      >
+        <h3>결과물 :</h3>
         <p>{result}</p>
       </div>
     </div>
